258b11b2cf2c8d33828c2cf9cfd542eb6c0d7921
Michael Fessenden <michael.fessenden@gmail.com>
2018-10-04 10:06:03 -0400

    v1.18 update

---
+++ js/retina-preprocess.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+   // jQuery methods go here...
+   console.log('Loading retina preprocessor...')
+   $('img').each(function(index, item) {
+       // string
+       var imgSrc = item.src
+       let fparts = imgSrc.split('.')
+
+       if (fparts.length == 2) {
+           let fname = fparts[0]
+           let fext = fparts[1]
+
+           if (fext == "png") {
+               console.log('-> image: ' + imgSrc)
+               $(item).attr( "data-rjs", "3" )
+           }
+
+       }
+
+    });
+});
+
+
+function fileExists(fileName) {
+    $.get(fileName, function(data, textStatus) {
+        if (textStatus == "success") {
+            // execute a success code
+            console.log("file loaded!");
+        }
+    });
+}
